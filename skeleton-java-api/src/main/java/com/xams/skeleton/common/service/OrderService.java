a470342ac6dd94c4cd54dc2e0e6e9efe6aeb0c65
xams-creator <1228552328@qq.com>
2021-05-13 16:39:52 +0800

    init: 初始化提交

---
+++ skeleton-java-api/src/main/java/com/xams/skeleton/common/service/OrderService.java
@@ -0,0 +1,9 @@
+package com.xams.skeleton.common.service;
+
+import com.xams.skeleton.server.entity.Order;
+
+public interface OrderService {
+
+    Order view();
+
+}
---
+++ skeleton-java-api/src/main/java/com/xams/skeleton/server/entity/Order.java
@@ -0,0 +1,14 @@
+package com.xams.skeleton.server.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Order {
+
+    private Long id;
+
+    private String code;
+
+}
